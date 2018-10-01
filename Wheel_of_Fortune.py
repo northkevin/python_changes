183f1ddaa3e436ea32cda4cb5efdf6dcf10e5879
Kevin <git@northkevin.com>
2018-09-30 17:50:32 -0700

    #4 - refactoring while loop into a for loop and seperating logic into more methods, adding constants for widely used strings

--- Wheel_of_Fortune.py
+++ Wheel_of_Fortune.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+BLANK_LETTER='_'
+
 
 def word_choice():
     """ Picks a word at random from the text file of potential word choices. """
@@ -12,10 +14,10 @@ def word_choice():
     return random.choice(choices)
 
 
-def blanks(w):
+def init_game_board(word):
     """ Creates a list of blank spaces to representing each letter in word w. """
-    blanks = ['_ '] * len(w)
-    return blanks
+    game_board = [BLANK_LETTER] * len(word)
+    return game_board
 
 
 def spin():
@@ -24,35 +26,50 @@ def spin():
     return random.choice(dollars)
 
 
-def display_current_winnings(word, blanks, guesses, winnings, turns):
+def display_board_and_current_winnings(game_board, winnings, turns_remaining):
     """Updates the user with their total winnings and the letters they have gotten correct."""
-    for (index, letter) in enumerate(word):
-        if letter in guesses:
-            blanks[index] = '{0} '.format(letter)
     print("Your total winnings are ${0} with {1} spin(s) to go\n"
           "Here's what you have right so far:\n\n"
           "{2}\n\n"
-          "Let's spin again!\n".format(winnings, turns, ''.join(blanks)))
+          "Let's spin again!\n".format(winnings, turns_remaining, ' '.join(game_board)))
 
+
+def get_number_of_remaining_turns(total_turns, current_turn):
+    return total_turns - current_turn
+
+
+# Lists are mutable.. the 'game_board' will be updated outside of this method.
+def update_board(game_board, actual_word, choice):
+    for pos, letter in enumerate(actual_word):
+        if choice.lower() == letter.lower():
+            game_board[pos] = choice.lower()
+
+
+def is_game_board_complete(game_board):
+    return BLANK_LETTER not in game_board
+
+
+# from sets import Set
 
 def wheel_of_fortune():
     """ Play Wheel of Fortune! """
 
     a = 0                               # Value for if the user guesses the word correctly.
     w = word_choice()
-    b = blanks(w)
-    turns = 5 + len(w)                  # How many turns the user gets.
+    game_board = init_game_board(w)
+    total_turns = 5 + len(w)            # How many turns the user gets.
     winnings = 0                        # Total number of winnings.
     guesses = []                        # List of all letters that have been guessed.
     vowels = ('a', 'e', 'i', 'o', 'u')
 
     print("Time to play some Wheel of Fortune!\n"
           "Your word today is:\n\n"
-          "{0}\n".format(''.join(b)))
+          "{0}\n".format(' '.join(game_board)))
     print("Let's spin the wheel!\n")
 
     # Play the game until the user runs out of turns.
-    while turns != 0 and '_ ' in b:
+    for current_turn in range(total_turns):
+        if is_game_board_complete(game_board): break
         s = spin()
         # If the user spins a 'bankrupt'
         if s == -1:
@@ -61,9 +78,8 @@ def wheel_of_fortune():
             winnings = 0
         # if the user spins a 'lose a turn'
         elif s == 0:
-            turns -= 1
             print("That's no fun, you lost a turn. You have {0} left.\n"
-                  "Let's spin again\n".format(turns))
+                  "Let's spin again\n".format(get_number_of_remaining_turns(total_turns, current_turn)))
         # If the user spins a dollar amount.
         elif s > 0:
             print("You landed on ${0}! It's time to guess a letter!\n"
@@ -81,27 +97,22 @@ def wheel_of_fortune():
                 # The user's attempt at guessing the word was wrong.
                 else:
                     print("\nSorry that isn't correct. Try again!")
-                    turns -= 1
             # The user guesses a letter they have already guessed.
             elif guess in guesses:
                 print("\nYou've already guessed that! Now you lose a turn.")
-                turns -= 1
             # The user guesses a vowel.
             elif guess in vowels:
                 # The user doesn't have enough to buy a vowel.
                 if winnings < 250:
                     print("\nSorry you don't have enough money to buy a vowel. You lose a turn")
-                    turns -= 1
                 # The user guesses a vowel that is in the word.
                 elif winnings > 250 and guess in w:
                     print("\nThat's correct!")
-                    turns -= 1
                     winnings -= 250
                     guesses.append(guess)
-
+                    update_board(game_board, w, guess)
                 else:
                     print("\nSorry, that's incorrect. Let's try again")
-                    turns -= 1
                     winnings -= 250
                     guesses.append(guess)
             # Any other letter is guessed.
@@ -110,15 +121,15 @@ def wheel_of_fortune():
                 if guess in w:
                     print("\nThat's correct!" )
                     winnings += s
-                    turns -= 1
                     guesses.append(guess)
+                    update_board(game_board, w, guess)
+
                 # The user guessed wrong.
                 else:
                     print("\nSorry, that's incorrect. Let's try again")
-                    turns -= 1
                     guesses.append(guess)
 
-            display_current_winnings(w, b, guesses, winnings, turns)
+            display_board_and_current_winnings(game_board, winnings, get_number_of_remaining_turns(total_turns, current_turn))
 
 
     # Outputs once the game is over
@@ -129,7 +140,7 @@ def wheel_of_fortune():
               "YOU'VE WON ${0}!\n"
               "Way to go!".format(winnings))
     # The user ran out of guesses.
-    elif '_ ' in b:
+    elif not is_game_board_complete(game_board):
         print("Looks like you ran out of guesses.\n"
               "The word was '{0}'\n"
               "Better luck next time!".format(w))
